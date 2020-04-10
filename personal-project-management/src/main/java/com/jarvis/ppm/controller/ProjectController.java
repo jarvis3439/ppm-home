b35b3526d4f84ffa072c9ef9295117e0980e81e6
jarvis <jarvis@tony>
2020-04-10 10:11:42 +0530

    custom exception for unique projectIdentifier

--- personal-project-management/src/main/java/com/jarvis/ppm/controller/ProjectController.java
+++ personal-project-management/src/main/java/com/jarvis/ppm/controller/ProjectController.java
@@ -1,22 +1,18 @@
 package com.jarvis.ppm.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jarvis.ppm.model.Project;
+import com.jarvis.ppm.service.ErrorValidationService;
 import com.jarvis.ppm.service.ProjectService;
 
 @RestController
@@ -26,17 +22,15 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 
+	@Autowired
+	private ErrorValidationService errorValidationService;
+
 	@PostMapping("")
 	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
-		if (result.hasErrors()) {
-			Map<String, String> errorMap = new HashMap<>();
-
-			for (FieldError error : result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-			}
-
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+		ResponseEntity<?> error = errorValidationService.validationService(result);
+		if (error != null) {
+			return error;
 		}
 
 		projectService.saveOrUpdateProject(project);
--- personal-project-management/src/main/java/com/jarvis/ppm/service/ProjectService.java
+++ personal-project-management/src/main/java/com/jarvis/ppm/service/ProjectService.java
@@ -3,6 +3,7 @@ package com.jarvis.ppm.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jarvis.ppm.exception.ProjectIdException;
 import com.jarvis.ppm.model.Project;
 import com.jarvis.ppm.repository.ProjectRepository;
 
@@ -11,9 +12,16 @@ public class ProjectService {
 
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	// save or update project
 	public Project saveOrUpdateProject(Project project) {
-		return projectRepository.save(project);
+
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exist");
+		}
+
 	}
 }
