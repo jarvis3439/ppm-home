b0c83d0953ddc4970b88b5c10e65298f48f7cbb2
jarvis3439 <harshpithadiya97@gmail.com>
2020-04-12 11:32:26 +0530

    project item and navigation bar component

--- personal-project-management-react-client/src/App.js
+++ personal-project-management-react-client/src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
-      <Dashboard />
+      <Header />
       <Dashboard />
     </div>
   );
 }
-
 export default App;
