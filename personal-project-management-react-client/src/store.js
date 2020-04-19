7d80cced1f73a0b8852c14d4ddd20102799310a5
jarvis3439 <harshpithadiya97@gmail.com>
2020-04-19 20:31:45 +0530

    created add project part through react

---
+++ personal-project-management-react-client/src/store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import { createElement } from "react";
+
+const initialState = {};
+
+const middleWare = [thunk];
+
+let store;
+
+if (window.navigator.userAgent.includes("Firefox")) {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleWare),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleWare))
+  );
+}
+
+export default store;
