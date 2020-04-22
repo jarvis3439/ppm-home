9b210b1cc0c326ee9cde1f5b0b1fd6b02e36bf55
jarvis3439 <harshpithadiya97@gmail.com>
2020-04-22 12:28:47 +0530

    update project tadk completed react-redux

--- personal-project-management/src/main/java/com/jarvis/ppm/model/Project.java
+++ personal-project-management/src/main/java/com/jarvis/ppm/model/Project.java
@@ -39,6 +39,7 @@ public class Project {
 	private Date end_date;
 
 	@JsonFormat(pattern = "yyyy-mm-dd")
+	@Column(updatable = false)
 	private Date createdAt;
 
 	@JsonFormat(pattern = "yyyy-mm-dd")
