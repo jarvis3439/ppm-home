49046595aace26514eb6ec71210cc32044826279
jarvis3439 <harshpithadiya97@gmail.com>
2020-04-28 16:44:37 +0530

    Routes to ProjectBoard and Add Project Task

--- personal-project-management-react-client/src/components/Project/ProjectItem.js
+++ personal-project-management-react-client/src/components/Project/ProjectItem.js
@@ -23,17 +23,17 @@ class ProjectItem extends Component {
             </div>
             <div className="col-md-4 d-none d-lg-block">
               <ul className="list-group">
-                <a href="#">
+                <Link to={`/projectBoard/${project.projectIdentifier}`}>
                   <li className="list-group-item board">
                     <i className="fa fa-flag-checkered pr-1"> Project Board </i>
                   </li>
-                </a>
+                </Link>
                 <Link to={`/updateProject/${project.projectIdentifier}`}>
                   <li className="list-group-item update">
                     <i className="fa fa-edit pr-1"> Update Project Info</i>
                   </li>
                 </Link>
-                <a>
+                <Link>
                   <li
                     className="list-group-item delete"
                     onClick={this.onDeleteClick.bind(
@@ -43,7 +43,7 @@ class ProjectItem extends Component {
                   >
                     <i className="fa fa-minus-circle pr-1"> Delete Project</i>
                   </li>
-                </a>
+                </Link>
               </ul>
             </div>
           </div>
