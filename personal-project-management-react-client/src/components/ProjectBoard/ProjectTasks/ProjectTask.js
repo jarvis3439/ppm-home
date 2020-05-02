b364e40c6a9af2e01f1a4c446a945494b8dce725
jarvis3439 <harshpithadiya97@gmail.com>
2020-05-02 12:14:48 +0530

    Update Project Task completed

--- personal-project-management-react-client/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
+++ personal-project-management-react-client/src/components/ProjectBoard/ProjectTasks/ProjectTask.js
@@ -25,14 +25,17 @@ class ProjectTask extends Component {
     return (
       <div className="card mb-1 bg-light">
         <div className={`card-header text-primary ${priorityClass}`}>
-          ID: {project_task.projectSequence} -- Priority: {priorityString}
+          ID: {project_task.projectSequence} || Priority: {priorityString}
         </div>
         <div className="card-body bg-light">
           <h5 className="card-title">{project_task.summary}</h5>
           <p className="card-text text-truncate ">
             {project_task.acceptanceCriteria}
           </p>
-          <Link to="" className="btn btn-primary">
+          <Link
+            to={`/updateProjectTask/${project_task.projectIdentifier}/${project_task.projectSequence}`}
+            className="btn btn-primary"
+          >
             View / Update
           </Link>
 
